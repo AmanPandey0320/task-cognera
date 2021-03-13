a5b3deb80bfa5382c337fc4e1aa9d04231527a52
AmanPandey0320 <72158086+AmanPandey0320@users.noreply.github.com>
2021-03-13 20:14:52 +0530

    initial commit

---
+++ server1/index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const http = require('http');
+const socketIO = require('socket.io');
+const socketioclient = require('socket.io-client');
+require('dotenv').config()
+const app = express()
+const server =  http.createServer(app);
+const port = process.env.PORT || 3000;
+
+const ioserver = socketIO(server, {
+    cors: {
+      origin: '*',
+    },
+});
+
+const ioclient = socketioclient('http://localhost:5400');
+
+ioclient.on('s2e1',msg =>{
+    console.log(msg);
+    ioclient.emit('s1e2','Hi! from server 2');
+});
+
+
+ioserver.on('connection', socket=>{
+    console.log('some one connected');
+
+    socket.emit('s1e1','Hi! from server 1');
+
+    socket.on('s2e1',msg=>{   //s1l1
+        console.log(msg);
+        socket.emit('s1e2','Hi! from server 2');
+    });
+
+    socket.on('s2e2',msg=>{ //s1l2
+        console.log(msg);
+    });
+});
+
+server.listen(port, () => console.log(`Example app listening on port port!`))
